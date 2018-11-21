347b5e2402ab34c4546df201b471cc4aad131461
Mark Zhang <markzhang.inbox@gmail.com>
2018-11-21 14:05:14 -0500

    Added Android App for simpler remote control

---
+++ androidapp/java/com/markyhzhang/poppyandroid/ConnectionListener.java
@@ -0,0 +1,65 @@
+package com.markyhzhang.poppyandroid;
+
+import android.support.design.widget.Snackbar;
+import android.widget.TextView;
+
+import okhttp3.Response;
+import okhttp3.WebSocket;
+import okhttp3.WebSocketListener;
+import okio.ByteString;
+
+public class ConnectionListener extends WebSocketListener{
+
+    private TextView tv;
+    private WebSocket server;
+    private boolean connected;
+    private Snackbar snackbar;
+
+    public boolean isConnected() {
+        return connected;
+    }
+
+    public ConnectionListener(TextView tv, Snackbar snackbar){
+        this.tv=tv;
+        this.snackbar = snackbar;
+    }
+
+    private static final int NORMAL_CLOSURE_STATUS = 1000;
+    @Override
+    public void onOpen(WebSocket webSocket, Response response) {
+        server = webSocket;
+        tv.setText("Status: Connected");
+        connected = true;
+    }
+    @Override
+    public void onMessage(WebSocket webSocket, String text) { }
+
+    @Override
+    public void onMessage(WebSocket webSocket, ByteString bytes) { }
+
+    @Override
+    public void onClosing(WebSocket webSocket, int code, String reason) {
+        webSocket.close(NORMAL_CLOSURE_STATUS, null);
+        connected = false;
+    }
+    @Override
+    public void onFailure(WebSocket webSocket, Throwable t, Response response) {
+        connected = false;
+        tv.setText("Status: Disconnected");
+        snackbar.setText("Server connection lost!").show();
+
+    }
+
+    void moveForwards(){
+        server.send("cmd moveForwards");
+    }
+
+    void moveBackwards(){
+        server.send("cmd moveBackwards");
+    }
+
+    void turn(int degrees) {
+        server.send("cmd turn " + degrees);
+    }
+
+}
---
+++ androidapp/java/com/markyhzhang/poppyandroid/MainActivity.java
@@ -0,0 +1,60 @@
+package com.markyhzhang.poppyandroid;
+
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.WebSocket;
+
+public class MainActivity extends AppCompatActivity {
+
+    private OkHttpClient client;
+    private Request request = new Request.Builder().url("ws://10.0.2.2:9001/socket").build();
+    private ConnectionListener connection;
+    private WebSocket ws;
+    private TextView tv;
+    private Snackbar snackbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        tv = findViewById(R.id.text_status);
+        connection = new ConnectionListener(tv, snackbar);
+        client = new OkHttpClient();
+        snackbar = Snackbar.make(findViewById(R.id.main_layout), "",
+                Snackbar.LENGTH_SHORT);
+    }
+
+    public void performAction(View view){
+        String t = ((TextView)view).getText().toString();
+        if (t.equals(getResources().getString(R.string.connect))) {
+            ws = client.newWebSocket(request, connection);
+            tv.setText("Status: Connecting...");
+        } else if (t.equals(getResources().getString(R.string.disconnect))) {
+            ws.close(1000, "bye");
+            tv.setText("Status: Disconnected");
+        }
+        if (connection.isConnected()) {
+            if (t.equals(getResources().getString(R.string.forward))) {
+                connection.moveForwards();
+            } else if (t.equals(getResources().getString(R.string.backward))) {
+                connection.moveBackwards();
+            } else if (t.equals(getResources().getString(R.string.left))) {
+                connection.turn(10);
+            } else if (t.equals(getResources().getString(R.string.right))) {
+                connection.turn(-10);
+            }
+        }else{
+            showSnackbar("Not connected to server!");
+        }
+    }
+
+    public void showSnackbar(String msg){
+        snackbar.setText(msg).show();
+    }
+}
